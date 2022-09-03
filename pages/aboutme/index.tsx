8575d861d5750f8492fdf26259693fc3b0720b45
yk-jp <k82.y12@gmail.com>
2022-09-03 01:10:00 -0700

    add base code

---
+++ pages/aboutme/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Aboutme: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>About me - Yusuke Portfolio</title>
+      </Head>
+
+      <h1 className="">Contact</h1>
+    </>
+  );
+};
+
+export default Aboutme;
---
+++ pages/contact/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact - Yusuke Portfolio</title>
+      </Head>
+
+      <h1 className="">Contact</h1>
+    </>
+  );
+};
+
+export default Contact;
