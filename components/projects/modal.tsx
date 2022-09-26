4cef67b84f2c95ecd1e12b3fa5a66746230401a5
yk-jp <k82.y12@gmail.com>
2022-09-25 22:51:02 -0700

    fix the layout for darkmode in project section

--- components/projects/modal.tsx
+++ components/projects/modal.tsx
@@ -13,11 +13,11 @@ const Modal = (props: any) => {
     >
       <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
         {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-800 dark:border-gray-600">
           {/* <!-- Modal header --> */}
           <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
             <div>
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-gray-300 mb-2">
                 {project.title}
               </h3>
               {project.devStyle && (
@@ -39,22 +39,22 @@ const Modal = (props: any) => {
           </div>
           {/* <!-- Modal body --> */}
           <div className="p-6 space-y-6">
-            <p className="mb-4 font-bold tracking-tight text-gray-900">
+            <p className="mb-4 font-bold tracking-tight text-gray-900 dark:text-gray-300">
               What I Learnt
             </p>
-            <p className="text-base leading-relaxed text-gray-500">
+            <p className="text-base leading-relaxed text-gray-900 dark:text-gray-400">
               {project.whatIlearnt}
             </p>
 
-            <p className="mb-4 font-bold tracking-tight text-gray-900">
+            <p className="mb-4 font-bold tracking-tight text-gray-900 dark:text-gray-300">
               Difficult Parts
             </p>
-            <p className="text-base leading-relaxed text-gray-500">
+            <p className="text-base leading-relaxed text-gray-900 dark:text-gray-400">
               {project.difficulties}
             </p>
             {project.techStack && (
               <>
-                <p className="mb-4 font-bold tracking-tight text-gray-900">
+                <p className="mb-4 font-bold tracking-tight text-gray-900 dark:text-gray-300">
                   Tech Stack
                 </p>
                 {project.techStack.map((name: String, idx: number) => {
@@ -74,14 +74,14 @@ const Modal = (props: any) => {
           <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200">
             {project.link.github && (
               <Link href={project.link.github}>
-                <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 mr-3 hover:text-blue-700">
+                <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 mr-3 hover:text-blue-700 dark:hover:text-blue-700 text-gray-900 dark:text-gray-300">
                   Github
                 </button>
               </Link>
             )}
             {project.link.live && (
               <Link href={project.link.live}>
-                <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 mr-3 hover:text-blue-700">
+                <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 mr-3 hover:text-blue-700 dark:hover:text-blue-700 text-gray-900 dark:text-gray-300">
                   Live
                 </button>
               </Link>
